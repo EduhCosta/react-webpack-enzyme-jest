fac63414de7ef0e334b611a596744b0221cf03d7
Eduh Costa <eduhcosta@live.com>
2018-04-13 19:57:16 -0300

    Configurando o lint

--- src/core/App.js
+++ src/core/App.js
@@ -1,15 +1,22 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 class App extends React.Component {
-  
-  render() {
-    return (
-      <React.Fragment>
-        <h1>Tá certo carai</h1>
-      </React.Fragment>
-    );
+  constructor (props) {
+    super(props);
+    this.state = { meuState: 'olá mundo' };
   }
 
+  render () {
+    return (
+      <div>
+        <h2>{this.props.titleApp}</h2>
+      </div>
+    );
+  }
 }
+
+App.defaultProps = { titleApp: 'My app' };
+App.propTypes = { titleApp: PropTypes.string };
 
 export default App;
